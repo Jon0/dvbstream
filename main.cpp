a29ad10249374c1d82b20bfb370b529c3a06475b
Jon0 <Jono4728@gmail.com>
2016-01-06 11:45:40 +1300

    error messages

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
 
 constexpr int buf_size = 1024 * 1024;
 
+int sendall(int outfd, char *buffer, int buf_size) {
+	while (buf_size > 0) {
+		buf_size -= write(outfd, buffer, buf_size);
+	}
+	return buf_size;
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		return 0;
@@ -15,12 +22,11 @@ int main(int argc, char *argv[]) {
 	// wait for connection
 	os::tcp_acceptor acceptor(8555);
 	os::tcp_socket sock(acceptor);
-	os::fdbuf buf(sock.fd());
-	std::iostream tcp_stream(&buf);
+	//os::fdbuf buf(sock.fd());
+	//std::iostream tcp_stream(&buf);
 
 	std::string path = argv[1];
 	std::cout << "opening " << path << "\n";
-
 	os::location loc(path);
 	int srcfd = loc.openfd();
 	char buffer[buf_size];
@@ -28,11 +34,19 @@ int main(int argc, char *argv[]) {
 	// transfer rate
 	int transferred = 0;
 	auto start_time = std::chrono::system_clock::now();
-	while (tcp_stream.good()) {
-		auto b = read(srcfd, buffer, buf_size);
-		tcp_stream.write(buffer, b);
+	while (true) {
+		int b = read(srcfd, buffer, buf_size);
+		if (b < 0) {
+			os::error("ERROR reading fd");
+		}
+		int s = sendall(sock.fd(), buffer, buf_size);
+		if (s < 0) {
+			os::error("ERROR writing fd");
+		}
+
+		// record transfer rate
 		transferred += b;
-		if (transferred > (1 << 20)) {
+		if (transferred > (1 << 16)) {
 			auto end_time = std::chrono::system_clock::now();
 			std::chrono::duration<double> elapsed_seconds = end_time - start_time;
 			auto rate = static_cast<double>(transferred) / (elapsed_seconds.count() * 1024 * 1024);
